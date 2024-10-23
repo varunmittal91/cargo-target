c31a40b071b443dfb301968dc6aff077db667c23
Varun Mittal <varunmittal91@gmail.com>
2024-10-23 17:41:42 -0400

    Updated lock

--- tags_parser/__init__.py
+++ tags_parser/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import TYPE_CHECKING, TypeAlias, Union
+from typing import TypeAlias, Union
 
 import polars as pl
 from polars.plugins import register_plugin_function
@@ -11,6 +11,7 @@ from tags_parser._internal import __version__ as __version__
 LIB = Path(__file__).parent
 
 IntoExprColumn: TypeAlias = Union[pl.Expr, str, pl.Series]
+
 
 def pig_latinnify(expr: IntoExprColumn) -> pl.Expr:
     return register_plugin_function(
